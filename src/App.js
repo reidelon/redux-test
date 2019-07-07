747e377688dc394d6cc5e6c19d3805d9951b626a
Reidel Lazaro Rodriguez Torres <reidelon@gmail.com>
2019-07-07 14:43:00 -0300

    added react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Route } from "react-router-dom";
+import { Route, Switch} from "react-router-dom";
 import HomePage from "./components/home/HomePage";
 import About from "./components/about/AboutPage";
+import Header from "./components/common/Header";
+import CoursesPage from "./components/courses/CoursesPage";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
   return (
-    <div className="Container-fluid">
-      <Route exact path="/" component={HomePage}/>
-      <Route path="about" component={About}/>
+    <div className="container">
+      <Header/>
+      <Switch>
+          <Route exact path="/" component={HomePage}/>
+          <Route path="/courses" component={CoursesPage}/>
+          <Route path="/about" component={About}/>
+          <Route component={PageNotFound}/>
+      </Switch>
     </div>
   );
 }
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -2,12 +2,15 @@ import React from "react";
 import { Link} from "react-router-dom";
 
 const HomePage = () => (
-    <div className="jumbutrom">
-        <h1>Administration</h1>
-        <p>React, Redux and React Router</p>
+    <div className="jumbotron">
+      <h1 className="display-4">Administration</h1>
+      <p className="lead">React, Redux and React Router.</p>
+      <hr className="my-4"/>
+      <p className="lead">
         <Link to="about" className="btn btn-primary btn-lg">
-            Learn more
+                Learn more
         </Link>
+      </p>
     </div>
 );
 
